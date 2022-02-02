dde4e293ac9ea346d199fc207c95e6a7c009dd94
sethflemingalho <sethflemingalho@gmail.com>
2022-02-02 13:40:37 -0800

    Detecting signals from a tv remote control

---
+++ ir_tv_remote_control.ino
@@ -0,0 +1,37 @@
+#include <IRremote.h>
+
+int receiver = 2;
+
+IRrecv irrecv(receiver);
+decode_results results;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("IR Receive Raw Data + Button Decode Test");
+  irrecv.enableIRIn();
+}
+
+void loop()
+{
+  if (irrecv.decode(&results));
+  {
+    Serial.print(results.value, HEX);
+    translateIR();
+    irrecv.resume();
+  }
+}
+
+void translateIR()
+{
+  switch(results.value)
+  {
+    
+    
+    default:
+    Serial.println(" = Not Programmed yet:)");
+  }
+  
+  delay(250);
+  
+}
